8fb036694c0059abfba699b551552bf367059904
pengjuan <pengjuan@starschina.com>
2019-07-30 10:10:47 +0800

    修改configHttp和logout，新增checkin脚本。

--- common/configHttp.py
+++ common/configHttp.py
@@ -90,7 +90,7 @@ class RunMain():
                         # 'Host': 'test.ams.starschina.com',
                         'Host': 'apiv1.starschina.com',
                         'Accept-Encoding': 'gzip',
-                        'token': self.get_login_token()
+                        'Authorization': self.get_login_token()
                         }
         return self.headers
 
@@ -108,6 +108,6 @@ class RunMain():
             logger.info("method值错误！！！")
         return result
 
-if __name__ == '__main__':#通过写死参数，来验证我们写的请求是否正确
-    result = RunMain().run_main('post', 'http://127.0.0.1:8888/login', 'name=xiaoming&pwd=')
-    print(result)
+# if __name__ == '__main__':#通过写死参数，来验证我们写的请求是否正确
+# #     result = RunMain().run_main('post', '', 'name=xiaoming&pwd=')
+# #     print(result)
--- testCase/ims/test_checkin.py
+++ testCase/ims/test_checkin.py
@@ -1,14 +1,14 @@
 # -*- coding:utf-8 -*-
-#@Time  : 2019/7/29 10:12
+#@Time  : 2019/7/30 9:05
 #@Author: pengjuan
-#@interfacetest: http://apiv1.starschina.com/ims/v1.0/user/logout
+#@interfacetest: http://apiv1.starschina.com/ims/v1.0/user/checkin
 
 from common.configHttp import RunMain
-from readConfig import ReadConfig
-from common.md5_sms import timeStamp_md5
-from datetime import datetime
 from common.getSign import get_Sign
-import unittest, json, requests, time
+from common.md5_sms import timeStamp_md5
+from readConfig import ReadConfig
+from datetime import datetime
+import unittest, requests, json, time
 
 global false, true, null
 baseurl = ReadConfig().get_http('baseurl')
@@ -17,17 +17,17 @@ app_key = ReadConfig().get_app('app_key')
 headers = RunMain().headers_token()
 md5 = timeStamp_md5()
 
-class test_Logout(unittest.TestCase):
-    """测试用户退出登录"""
+class test_Checkin(unittest.TestCase):
+    """测试用户签到"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.url = baseurl + '/ims/v1.0/user/logout'
+        self.url = baseurl + '/ims/v1.0/user/checkin'
         self.timeStamp = int(time.mktime(datetime.now().timetuple()))
         self.access_token = md5.encrypt_md5(self.timeStamp)
 
-    def test_logout_01(self):
-        """正确的退出登录参数"""
+    def test_checkin_01(self):
+        """正确的签到参数"""
         data = '{"app_version":"%(version)s",' \
                '"access_token":"%(access_token)s",' \
                '"os_type":1,' \
@@ -35,7 +35,6 @@ class test_Logout(unittest.TestCase):
                '"provider":1,' \
                '"app_key":"%(app_key)s",' \
                '"device_id":"802ca0fba119ab0a",' \
-               '"country_code":"+86",' \
                '"installation_id":1904301718321742,' \
                '"longitude":108.90823353286173,' \
                '"latitude":34.21936825217505,' \
@@ -48,22 +47,19 @@ class test_Logout(unittest.TestCase):
         response = requests.post(self.url, data=json.dumps(data), headers=headers)
         if response.status_code == 200:
             response_data = response.json()
-            assert response_data['err_code'] == 0
+            assert response_data['err_code'] == 0 and response_data['data']['already_checkin'] == True
         else:
             print("获取%s接口返回的参数错误" % self.url)
 
-
-    def test_logout_02(self):
-        """错误的退出登录参数"""
-
+    def test_checkin_02(self):
+        """错误的签到参数"""
         data = '{"app_version":"%(version)s",' \
                '"access_token":"%(access_token)s",' \
                '"os_type":1,' \
                '"timestamp":%(timeStamp)d,' \
                '"provider":1,' \
-               '"app_key":"ddfsweer",' \
+               '"app_key":"ffdsggsfar",' \
                '"device_id":"802ca0fba119ab0a",' \
-               '"country_code":"+86",' \
                '"installation_id":1904301718321742,' \
                '"longitude":108.90823353286173,' \
                '"latitude":34.21936825217505,' \
@@ -79,22 +75,19 @@ class test_Logout(unittest.TestCase):
         else:
             print("接口%s请求os_type参数值错误，返回的err_code应为500" % self.url)
 
-    def test_logout_03(self):
-        """空的退出登录参数"""
-
+    def test_checkin_03(self):
+        """空的签到参数"""
         data = '{"app_version":"%(version)s",' \
                '"access_token":"%(access_token)s",' \
+               '"os_type":1,' \
                '"timestamp":%(timeStamp)d,' \
                '"provider":1,' \
-               '"app_key":"%(app_key)s",' \
                '"device_id":"802ca0fba119ab0a",' \
-               '"country_code":"+86",' \
                '"installation_id":1904301718321742,' \
                '"longitude":108.90823353286173,' \
                '"latitude":34.21936825217505,' \
                '}' % {
                    'version': version,
-                   'app_key': app_key,
                    'access_token': self.access_token,
                    'timeStamp': self.timeStamp}
         data = get_Sign().encrypt(data)
@@ -104,9 +97,10 @@ class test_Logout(unittest.TestCase):
             assert err_code == 500
         else:
             print("接口%s请求os_type参数值错误，返回的err_code应为500" % self.url)
+
 
 if __name__ == "__main__":
 
-    test_Logout().test_logout_01()
-    test_Logout().test_logout_02()
-    test_Logout().test_logout_03()
+    test_Checkin().test_checkin_01()
+    test_Checkin().test_checkin_02()
+    test_Checkin().test_checkin_03()

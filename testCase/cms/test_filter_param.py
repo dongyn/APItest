a5b35089a59e8eb58df8ba1246a64c5ceffe87ac
dongyani <dongyani@starschina.com>
2019-08-02 17:03:07 +0800

    1.调试filterparam接口

--- testCase/cms/test_filter_param.py
+++ testCase/cms/test_filter_param.py
@@ -14,21 +14,20 @@ global false, true, null
 baseurl = ReadConfig().get_http('baseurl')
 version = ReadConfig().get_app('version')
 app_key = ReadConfig().get_app('app_key')
-md5 = timeStamp_md5()
-headers= RunMain().headers()
+headers= RunMain().headers_get()
 
-class test_Orderstatus(unittest.TestCase):
+class test_Filterparam(unittest.TestCase):
     """测试订单状态"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.url = baseurl + '/cms/v1.0/filter/param'
-        self.timeStamp = int(time.mktime(datetime.now().timetuple()))
 
     def get_url_params(self):
         timeStamp = int(time.mktime(datetime.now().timetuple()))
         # category_id 必填
         data = '{"os_type":1,'\
+               '"os_version":9,'\
                '"app_version":"%(version)s",' \
                '"category_id":1,' \
                '"timestamp":%(timeStamp)d,' \
@@ -40,7 +39,7 @@ class test_Orderstatus(unittest.TestCase):
         data = get_Sign().encrypt(data)
         return RunMain().get_url_params(data, self.url)
 
-    def test_orderstatus_01(self):
+    def test_filterparam_01(self):
         """正确的参数"""
         url = self.get_url_params()
         response = requests.get(url, headers=headers)
@@ -49,16 +48,16 @@ class test_Orderstatus(unittest.TestCase):
         assert data['properties'][0]['name'] == '地域'
 
 
-    def test_orderstatus_02(self):
+    def test_filterparam_02(self):
         """错误的参数"""
         url = self.get_url_params().replace("category_id=1", "category_id=a")
         response = requests.get(url, headers=headers)
         assert response.json()['err_code']==500
 
-    def test_orderstatus_03(self):
+    def test_filterparam_03(self):
         """参数为空"""
         response = requests.get(self.url, headers=headers)
         assert response.json()['err_code']==500
 
 # if __name__ == "__main__":
-#     test_Orderstatus().test_orderstatus_01()
+#     test_filterparam().test_filterparam_01()

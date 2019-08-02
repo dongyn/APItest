85f6ea0f5cedaae64255de475d0a698806a89262
dongyani <dongyani@starschina.com>
2019-08-02 17:13:56 +0800

    1.新增get请求的headers_get

--- common/configHttp.py
+++ common/configHttp.py
@@ -57,6 +57,13 @@ class RunMain():
                    }
         return headers
 
+    def headers_get(self):
+        # "User-Agent": "Mozilla/5.0 (Linux; Android 9; COL-AL10 Build/HUAWEICOL-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/72.0.3626.121 Mobile Safari/537.36",
+        headers = {"Host": "apiv1.starschina.com",
+                    "Connection": "Keep-Alive",
+                    "Accept-Encoding": "gzip"
+                   }
+        return headers
     # 将解密后的字符串转为字典
     def decrypt_to_dict(self, text, key_type):
         r_data = text.json()['data']

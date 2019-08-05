5609c4f659f95013d2abd8f26b44e44793500c14
dongyani <dongyani@starschina.com>
2019-08-05 09:09:22 +0800

    增加get请求的headers中带token的函数：

--- common/configHttp.py
+++ common/configHttp.py
@@ -58,7 +58,6 @@ class RunMain():
         return headers
 
     def headers_get(self):
-        # "User-Agent": "Mozilla/5.0 (Linux; Android 9; COL-AL10 Build/HUAWEICOL-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/72.0.3626.121 Mobile Safari/537.36",
         headers = {"Host": "apiv1.starschina.com",
                     "Connection": "Keep-Alive",
                     "Accept-Encoding": "gzip"
@@ -120,6 +119,14 @@ class RunMain():
                    'Host': 'apiv1.starschina.com',
                    'Accept-Encoding': 'gzip',
                    'Authorization': self.get_login_token(timeStamp=timeStamp)
+                   }
+        return headers
+
+    def headers_get_token(self, timeStamp):
+        headers = {"Host": "apiv1.starschina.com",
+                    "Connection": "Keep-Alive",
+                    "Accept-Encoding": "gzip",
+                   "Authorization": self.get_login_token(timeStamp=timeStamp)
                    }
         return headers
 

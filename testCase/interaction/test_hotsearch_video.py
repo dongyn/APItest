efe400591975809f8c472ab00fdd9bb7f21c8b43
dongyani <dongyani@starschina.com>
2019-08-07 17:51:35 +0800

    对循环的测试用例做了动态生成

--- testCase/interaction/test_hotsearch_video.py
+++ testCase/interaction/test_hotsearch_video.py
@@ -1,3 +1,4 @@
+# !/usr/bin/env python
 # -*- coding:utf-8 -*-
 # @Time  : 2019/8/5 16:32
 # @Author: dongyani
@@ -68,25 +69,51 @@ class test_hotsearch_video(unittest.TestCase):
                                       "title": hotsearch["title"]})
         return hotsearch_content
 
-    def test_hotsearch_list_video(self):
-        for bookmark in list(self.bookmark_dict.keys()):
-            hotsearch_content = self.get_hotsearch_list_content(self.bookmark_dict[bookmark])
-            for content in hotsearch_content:
-                data = '{"content_id": %(content_id)d, ' \
-                       '"content_type": %(content_type)d, ' \
-                       '"os_type": 1, ' \
-                       '"app_version": "%(version)s", ' \
-                       '"app_key":"%(app_key)s"}' % {
-                           'content_id': content["content_id"],
-                           'content_type': content["content_type"],
-                           'version': version,
-                           'app_key': app_key}
-                crypt_data = aes.encrypt(data, 'c_q')
-                form = {"data": crypt_data, "encode": "v1"}
-                headers = RunMain().headers()
-                url = baseurl + "/cms/v1.2/video"
-                response = requests.post(url=url, data=json.dumps(form), headers=headers)
-                msg = "搜索页面{0}页签的{1}视频无法点播".format(bookmark, content["title"])
-                self.assertEqual(response.status_code, 200, msg=msg)
-                response_data = RunMain().decrypt_to_dict(response, 'r')
-                self.assertEqual(response_data['id'], content["content_id"], msg=msg)
+    def hotsearch_list_video(self, content, bookmark):
+            data = '{"content_id": %(content_id)d, ' \
+                   '"content_type": %(content_type)d, ' \
+                   '"os_type": 1, ' \
+                   '"app_version": "%(version)s", ' \
+                   '"app_key":"%(app_key)s"}' % {
+                       'content_id': content["content_id"],
+                       'content_type': content["content_type"],
+                       'version': version,
+                       'app_key': app_key}
+            crypt_data = aes.encrypt(data, 'c_q')
+            form = {"data": crypt_data, "encode": "v1"}
+            headers = RunMain().headers()
+            url = baseurl + "/cms/v1.2/video"
+            response = requests.post(url=url, data=json.dumps(form), headers=headers)
+            msg = "搜索页面{0}页签的{1}视频无法点播".format(bookmark, content["title"])
+            self.assertEqual(200, response.status_code, msg=msg)
+            response_data = RunMain().decrypt_to_dict(response, 'r')
+            self.assertEqual(content["content_id"], response_data['id'], msg=msg)
+
+    @staticmethod
+    def getTestFunc(content, bookmark):
+        def func(self):
+            self.hotsearch_list_video(content, bookmark)
+        return func
+
+def __generateTestCases():
+    bookmark_dict = {"猜你想搜": "%E7%8C%9C%E4%BD%A0%E6%83%B3%E6%90%9C",
+                     "电影": "%E7%94%B5%E5%BD%B1",
+                     "新闻": "%E6%96%B0%E9%97%BB",
+                     "电视剧": "%E7%94%B5%E8%A7%86%E5%89%A7",
+                     "体育": "%E4%BD%93%E8%82%B2",
+                     "CRI": "CRI",
+                     "短视频": "%E7%9F%AD%E8%A7%86%E9%A2%91",
+                     "综艺": "%E7%BB%BC%E8%89%BA",
+                     "财经": "%E8%B4%A2%E7%BB%8F",
+                     "动漫": "%E5%8A%A8%E6%BC%AB"
+                     }
+    for bookmark in list(bookmark_dict.keys()):
+        hotsearch_content = test_hotsearch_video().get_hotsearch_list_content(bookmark_dict[bookmark])
+        for content in hotsearch_content:
+            setattr(test_hotsearch_video, 'test_func_%s_%s' % (bookmark, content["title"]),
+                    test_hotsearch_video.getTestFunc(content, bookmark))
+
+__generateTestCases()
+
+if __name__ == '__main__':
+    unittest.main()
--- testCase/interaction/test_search_stream.py
+++ testCase/interaction/test_search_stream.py
@@ -37,27 +37,39 @@ class test_search_stream(unittest.TestCase):
             'select stream.id, stream.title from stream LEFT JOIN resource_param on stream.id = resource_param.content_id '
             'where resource_param.online = 1 and resource_param.app_id = 1 and resource_param.content_type = 4;')
 
-    def test_Viewlivestreamdetails(self):
-        stream_list = self.get_sql_list()
-        for stream in stream_list:
-            url = self.baseurl + "/cms/v1.2/stream"
-            data = '{"os_type":1, ' \
-                   '"app_version":"%(version)s", ' \
-                   '"id":%(stream_id)d, ' \
-                   '"app_key":"%(app_key)s"}' % {
-                'version': self.version,
-                'app_key': self.app_key,
-                'stream_id': stream["id"]}
-            crypt_data = self.aes.encrypt(data, 'c_q')
-            form = {"data": crypt_data, "encode": "v1"}
-            response = requests.post(url=url, data=json.dumps(form), headers=self.headers)
-            response_data = RunMain().decrypt_to_dict(response, 'r')
-            msg = "直播接口返回的电视台名称应该是{0}实际是{1}".format(stream["title"], response_data["title"])
-            self.assertEqual(stream["title"], response_data["title"], msg=msg)
+    def Viewlivestreamdetails(self, stream):
+        url = self.baseurl + "/cms/v1.2/stream"
+        data = '{"os_type":1, ' \
+               '"app_version":"%(version)s", ' \
+               '"id":%(stream_id)d, ' \
+               '"app_key":"%(app_key)s"}' % {
+                   'version': self.version,
+                   'app_key': self.app_key,
+                   'stream_id': stream["id"]}
+        crypt_data = self.aes.encrypt(data, 'c_q')
+        form = {"data": crypt_data, "encode": "v1"}
+        response = requests.post(url=url, data=json.dumps(form), headers=self.headers)
+        response_data = RunMain().decrypt_to_dict(response, 'r')
+        msg = "直播接口返回的电视台名称应该是{0}实际是{1}".format(stream["title"], response_data["title"])
+        self.assertEqual(stream["title"], response_data["title"], msg=msg)
+
+    @staticmethod
+    def getTestFunc(stream):
+        def func(self):
+            self.Viewlivestreamdetails(stream)
+        return func
+
+def __generateTestCases():
+    stream_list = test_search_stream().get_sql_list()
+    for stream in stream_list:
+        setattr(test_search_stream, 'test_func_%s' % (stream["title"]),
+                test_search_stream.getTestFunc(stream))
 
 
-# if __name__ == "__main__":
-#     test_search_stream().get_sql_list()
+__generateTestCases()
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 

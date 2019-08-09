012abf906e5e5b6a402772069f1e044852ef3058
dongyani <dongyani@starschina.com>
2019-08-09 15:04:33 +0800

    1.通过版本号控制case是否运行

--- testCase/cms/test_accesskey.py
+++ testCase/cms/test_accesskey.py
@@ -1,21 +1,20 @@
 # -*- coding:utf-8 -*-
-#@Time  : 2019/7/10 10:23
-#@Author: dongyani
-#@interfacetest: http://apiv1.starschina.com/cms/v1.0/funtv/accesskey
+# @Time  : 2019/7/10 10:23
+# @Author: dongyani
+# @interfacetest: http://apiv1.starschina.com/cms/v1.0/funtv/accesskey
 
-import unittest
-import requests,json
+import requests, unittest
 from common.AES_CBC import AES_CBC
 from readConfig import ReadConfig
 from common.configHttp import RunMain
 
-global false, null, true
-
 baseurl = ReadConfig().get_http("baseurl")
 version = ReadConfig().get_app("version")
 app_key = ReadConfig().get_app("app_key")
+funtv_version = ["8.0.1", "8.0.2", "8.0.3", "8.0.4", "8.0.5", "8.0.6"]
 headers = RunMain().headers_get()
 aes = AES_CBC()
+
 
 class funtv_accesskey(unittest.TestCase):
     """测试风行缓存接口"""
@@ -25,7 +24,7 @@ class funtv_accesskey(unittest.TestCase):
         self.url = baseurl + "/cms/v1.0/funtv/accesskey"
 
     def get_url_param(self):
-        #cp: fiag64t; secret_key: !N6nYXm9i@V 这两个参数是必传的
+        # cp: fiag64t; secret_key: !N6nYXm9i@V 这两个参数是必传的
         data = '{"cp":"fiag64t",' \
                '"secret_key":"!N6nYXm9i@V",' \
                '"app_version":"%(version)s",' \
@@ -35,24 +34,27 @@ class funtv_accesskey(unittest.TestCase):
                    'app_key': app_key}
         return RunMain().get_url_params(data, self.url)
 
-
+    @unittest.skipIf(version not in funtv_version, '8.0.7以后的版本不运行风行缓存接口')
     def test_01_funtv_accesskey(self):
         '''正确的请求参数'''
-        response = requests.get(url = self.get_url_param(), headers = headers)
-        assert response.json()['data']['access_key'] == "ZmlhZzY0dCwxNTY0Mzk0MjY1LGIzZjBhOTllNDZjOWMxZTNkMDI0NmUwMWQxNjUzNDgz"
+        response = requests.get(url=self.get_url_param(), headers=headers)
+        assert len(response.json()['data']['access_key']) > 20 & response.json()['err_code'] == 0
 
+    @unittest.skipIf(version not in funtv_version, '8.0.7以后的版本不运行风行缓存接口')
     def test_02_funtv_accesskey_cp_error(self):
         '''请求参数cp的值错误'''
         url = self.get_url_param().replace('cp=fiag64t', 'cp=fiag32taaa')
         response = requests.get(url=url, headers=headers)
         assert response.json()['err_code'] == 500
 
+    @unittest.skipIf(version not in funtv_version, '8.0.7以后的版本不运行风行缓存接口')
     def test_03_funtv_accesskey_secret_key_error(self):
         '''请求参数secret_key的值错误'''
         url = self.get_url_param().replace('secret_key=!N6nYXm9i@V', 'secret_key=!N6nYXm9iaaa')
         response = requests.get(url=url, headers=headers)
         assert response.json()['err_code'] == 500
 
+    @unittest.skipIf(version not in funtv_version, '8.0.7以后的版本不运行风行缓存接口')
     def test_04_funtv_accesskey_null(self):
         '''请求参数cp与secret_key值为空'''
         url = self.get_url_param().replace('cp=fiag64t', 'cp=')
@@ -61,5 +63,5 @@ class funtv_accesskey(unittest.TestCase):
         assert response.status_code == 404
 
 # if __name__ == "__main__":
+#     print(version)
 #     config_app_scret_key().test_01_config_correct()
-

baf4af7fac3bd0c7e02980ba343f6d7b39840a26
dongyani <dongyani@starschina.com>
2019-08-15 10:23:41 +0800

    1.修改请求参数和验证逻辑

--- testCase/cms/test_researchreport_list.py
+++ testCase/cms/test_researchreport_list.py
@@ -6,8 +6,10 @@
 from common.configHttp import RunMain
 from readConfig import ReadConfig
 from common.configMysql import OperationDbInterface
+from common.getSign import get_Sign
 from common.AES_CBC import AES_CBC
-import requests, unittest, json
+from datetime import datetime
+import requests, unittest, json, time
 
 baseurl = ReadConfig().get_http('baseurl')
 version = ReadConfig().get_app('version')
@@ -28,38 +30,63 @@ class test_Researchreport_list(unittest.TestCase):
 
     def test_researchreport_list_01(self):
         """参数有tycoon_id，返回大咖主页研报"""
+        timeStamp = int(time.mktime(datetime.now().timetuple()))
         data = '{"app_version":"%(version)s",' \
                '"os_type":1,' \
+               '"os_version":"9",' \
+               '"mac_address":"02:00:00:00:00:00",' \
+               '"device_id":"802ca0fba119ab0a",' \
                '"tycoon_id":%(tycoon_id)d,' \
                '"query_type":"all",' \
+               '"timestamp":%(timeStamp)d,' \
                '"app_key":"%(app_key)s"' \
                '}' % {
                    'version': version,
                    'tycoon_id': self.researchreport_detail["tycoon_id"],
+                   'timeStamp': timeStamp,
                    'app_key': app_key}
+        sign = get_Sign().encrypt(data, True)["sign"]
+        data = data.replace('}', ',"sign":"%s"}' % sign)
         crypt_data = aes.encrypt(data, 'c_q')
         form = {'data': crypt_data, 'encode': 'v1'}
         response = requests.post(self.url, data=json.dumps(form), headers=headers)
-        researchreport_title = RunMain().decrypt_to_dict(response, 'r')["free"][0]["title"]
-        msg = "大咖{0}研报应是应该是{1}".format(self.researchreport_detail["tycoon_name"],
+        researchreport_all = RunMain().decrypt_to_dict(response, 'r')
+        print(researchreport_all)
+        # - charge 列表为收费研报
+        # - free  列表为免费研报
+        title_list = []
+        for free_page in ["free", "charge"]:
+            if free_page in researchreport_all.keys():
+                for researchreport in researchreport_all[free_page]:
+                    title_list.append(researchreport["title"])
+        print(title_list)
+        msg = "大咖id为{0}，大咖的研报应该包含{1}".format(self.researchreport_detail["tycoon_id"],
                                        self.researchreport_detail["title"])
-        self.assertEqual(self.researchreport_detail["title"], researchreport_title, msg)
+        self.assertIn(self.researchreport_detail["title"], title_list, msg)
 
     def test_researchreport_list_02(self):
         """researchreport_id参数为错误"""
+        timeStamp = int(time.mktime(datetime.now().timetuple()))
         data = '{"app_version":"%(version)s",' \
                '"os_type":1,' \
+               '"os_version":"9",' \
+               '"mac_address":"02:00:00:00:00:00",' \
+               '"device_id":"802ca0fba119ab0a",' \
                '"tycoon_id":%(tycoon_id)d,' \
+               '"timestamp":%(timeStamp)d,' \
                '"app_key":"%(app_key)s"' \
                '}' % {
                    'version': version,
-                   'tycoon_id': -1,
+                   'tycoon_id': 10000000000,
+                   'timeStamp': timeStamp,
                    'app_key': app_key}
+        sign = get_Sign().encrypt(data, True)["sign"]
+        data = data.replace('}', ',"sign":"%s"}' % sign)
         crypt_data = aes.encrypt(data, 'c_q')
         form = {'data': crypt_data, 'encode': 'v1'}
         response = requests.post(self.url, data=json.dumps(form), headers=headers)
-        self.assertEqual(400, response.status_code, "大咖的id参数错误，研报接口状态码应为400")
-        self.assertEqual(500, response.json()["err_code"], "大咖的id参数错误，研报接口应返回err_code为500")
+        self.assertEqual(500, response.json()["err_code"], "大咖的id参数错误，接口应返回err_code为500")
+
 
     def get_researchreport_title(self):
         titles = []
@@ -68,13 +95,21 @@ class test_Researchreport_list(unittest.TestCase):
         return titles
 
     def get_researchreport_list(self):
+        timeStamp = int(time.mktime(datetime.now().timetuple()))
         data = '{"app_version":"%(version)s",' \
                '"os_type":1,' \
+               '"os_version":"9",' \
+               '"mac_address":"02:00:00:00:00:00",' \
+               '"device_id":"802ca0fba119ab0a",' \
                '"query_type":"all",' \
+               '"timestamp":%(timeStamp)d,' \
                '"app_key":"%(app_key)s"' \
                '}' % {
                    'version': version,
+                   'timeStamp': timeStamp,
                    'app_key': app_key}
+        sign = get_Sign().encrypt(data, True)["sign"]
+        data = data.replace('}', ',"sign":"%s"}' % sign)
         crypt_data = aes.encrypt(data, 'c_q')
         form = {'data': crypt_data, 'encode': 'v1'}
         response = requests.post(self.url, data=json.dumps(form), headers=headers)
--- testCase/cms/test_tycoon_list.py
+++ testCase/cms/test_tycoon_list.py
@@ -6,8 +6,10 @@
 from common.configHttp import RunMain
 from readConfig import ReadConfig
 from common.configMysql import OperationDbInterface
+from common.getSign import get_Sign
 from common.AES_CBC import AES_CBC
-import requests, unittest, json
+from datetime import datetime
+import requests, unittest, json, time
 
 baseurl = ReadConfig().get_http('baseurl')
 version = ReadConfig().get_app('version')
@@ -28,15 +30,23 @@ class test_Tycoon_list(unittest.TestCase):
 
     def test_tycoon_list_01(self):
         """参数有tycoon_id，返回单个大咖信息"""
+        timeStamp = int(time.mktime(datetime.now().timetuple()))
         data = '{"app_version":"%(version)s",' \
                '"os_type":1,' \
                '"tycoon_id":%(tycoon_id)d,' \
+               '"os_version":"9",' \
+               '"mac_address":"02:00:00:00:00:00",'\
+               '"device_id":"802ca0fba119ab0a",' \
                '"limit":1,' \
+               '"timestamp":%(timeStamp)d,' \
                '"app_key":"%(app_key)s"' \
                '}' % {
                    'version': version,
                    'tycoon_id': self.tycoon_detail["id"],
+                   'timeStamp': timeStamp,
                    'app_key': app_key}
+        sign = get_Sign().encrypt(data, True)["sign"]
+        data = data.replace('}', ',"sign":"%s"}' % sign)
         crypt_data = aes.encrypt(data, 'c_q')
         form = {'data': crypt_data, 'encode': 'v1'}
         response = requests.post(self.url, data=json.dumps(form), headers=headers)
@@ -48,17 +58,22 @@ class test_Tycoon_list(unittest.TestCase):
         data = '{"app_version":"%(version)s",' \
                '"os_type":1,' \
                '"tycoon_id":%(tycoon_id)d,' \
+               '"os_version":"9",' \
+               '"mac_address":"02:00:00:00:00:00",' \
+               '"device_id":"802ca0fba119ab0a",' \
                '"limit":1,' \
                '"app_key":"%(app_key)s"' \
                '}' % {
                    'version': version,
                    'tycoon_id': -1,
                    'app_key': app_key}
+        sign = get_Sign().encrypt(data, True)["sign"]
+        data = data.replace('}', ',"sign":"%s"}' % sign)
         crypt_data = aes.encrypt(data, 'c_q')
         form = {'data': crypt_data, 'encode': 'v1'}
         response = requests.post(self.url, data=json.dumps(form), headers=headers)
-        self.assertEqual(400, response.status_code, "大咖的id参数错误，接口状态码应为400")
-        self.assertEqual(500, response.json()["err_code"], "大咖的id参数错误，接口应返回err_code为500")
+        self.assertEqual(10032, response.json()["err_code"], "大咖的id参数错误，接口应返回err_code为500")
+        self.assertEqual("无效的签名", response.json()["err_msg"], "大咖的id参数错误，接口应返回无效的签名")
 
     def get_tycoon_name(self):
         names = []
@@ -67,14 +82,22 @@ class test_Tycoon_list(unittest.TestCase):
         return names
 
     def get_tycoon_list(self):
+        timeStamp = int(time.mktime(datetime.now().timetuple()))
         data = '{"app_version":"%(version)s",' \
                '"os_type":1,' \
                '"limit":5,' \
+               '"os_version":"9",' \
+               '"mac_address":"02:00:00:00:00:00",' \
+               '"device_id":"802ca0fba119ab0a",' \
+               '"timestamp":%(timeStamp)d,' \
                '"app_key":"%(app_key)s"' \
                '}' % {
                    'version': version,
                    'tycoon_id': self.tycoon_detail["id"],
+                   'timeStamp': timeStamp,
                    'app_key': app_key}
+        sign = get_Sign().encrypt(data, True)["sign"]
+        data = data.replace('}', ',"sign":"%s"}' % sign)
         crypt_data = aes.encrypt(data, 'c_q')
         form = {'data': crypt_data, 'encode': 'v1'}
         response = requests.post(self.url, data=json.dumps(form), headers=headers)
